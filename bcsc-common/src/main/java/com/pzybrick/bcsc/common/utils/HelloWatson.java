1c5f3d08e1739df1db06eef5520d055d16ef981c
Pete Zybrick <pzybrick@gmail.com>
2018-04-16 09:53:32 -0400

    setup

---
+++ bcsc-common/src/main/java/com/pzybrick/bcsc/common/utils/HelloWatson.java
@@ -0,0 +1,10 @@
+package com.pzybrick.bcsc.common.utils;
+
+public class HelloWatson {
+
+	public static void main(String[] args) {
+		System.out.println("Watson, come here. I need you.");
+
+	}
+
+}
