2c7294c36fd754aff51a930905849bac1fdad5f7
Pete Zybrick <pzybrick@gmail.com>
2018-05-18 15:45:36 -0400

    updates

--- bcsc-service/src/main/java/com/petezybrick/bcsc/service/orc/OrcSchemaMgr.java
+++ bcsc-service/src/main/java/com/petezybrick/bcsc/service/orc/OrcSchemaMgr.java
@@ -11,81 +11,91 @@ public class OrcSchemaMgr {
 	public static final Map<String,TypeDescription> mapOrcSchemas = new HashMap<String,TypeDescription>();
 	
 	static {
-		mapOrcSchemas.put("supplier|1.0", TypeDescription.createStruct()
-			     .addField("supplier_uuid", TypeDescription.createString())
-			     .addField("duns_number", TypeDescription.createString())
-			     .addField("supplier_name", TypeDescription.createString())
-			     .addField("supplier_category", TypeDescription.createString())
-			     .addField("supplier_sub_category", TypeDescription.createString())
-			     .addField("state_province", TypeDescription.createString())
-			     .addField("country", TypeDescription.createString())
-			     .addField("encoded_public_key", TypeDescription.createString())
-
-
-			     );
-
-			mapOrcSchemas.put("supplier_blockchain|1.0", TypeDescription.createStruct()
-			     .addField("supplier_blockchain_uuid", TypeDescription.createString())
-			     .addField("supplier_type", TypeDescription.createString())
-
-
-			     );
-
-			mapOrcSchemas.put("supplier_block|1.0", TypeDescription.createStruct()
-			     .addField("supplier_block_uuid", TypeDescription.createString())
-			     .addField("supplier_blockchain_uuid", TypeDescription.createString())
-			     .addField("hash", TypeDescription.createString())
-			     .addField("previous_hash", TypeDescription.createString())
-			     .addField("block_timestamp", TypeDescription.createTimestamp())
-			     .addField("block_sequence", TypeDescription.createInt())
-
-
-			     );
-
-			mapOrcSchemas.put("supplier_block_transaction|1.0", TypeDescription.createStruct()
-			     .addField("supplier_block_transaction_uuid", TypeDescription.createString())
-			     .addField("supplier_block_uuid", TypeDescription.createString())
-			     .addField("transaction_id", TypeDescription.createString())
-			     .addField("encoded_public_key_from", TypeDescription.createString())
-			     .addField("encoded_public_key_to", TypeDescription.createString())
-			     .addField("signature", TypeDescription.createBinary())
-			     .addField("transaction_sequence", TypeDescription.createInt())
-
-
-			     );
-
-			mapOrcSchemas.put("supplier_transaction|1.0", TypeDescription.createStruct()
-			     .addField("supplier_transaction_uuid", TypeDescription.createString())
-			     .addField("supplier_block_transaction_uuid", TypeDescription.createString())
-			     .addField("supplier_uuid", TypeDescription.createString())
-			     .addField("supplier_lot_number", TypeDescription.createString())
-			     .addField("item_number", TypeDescription.createString())
-			     .addField("description", TypeDescription.createString())
-			     .addField("qty", TypeDescription.createInt())
-			     .addField("units", TypeDescription.createString())
-			     .addField("shipped_date_iso8601", TypeDescription.createTimestamp())
-			     .addField("rcvd_date_iso8601", TypeDescription.createTimestamp())
-
-
-			     );
-
-			mapOrcSchemas.put("lot_canine|1.0", TypeDescription.createStruct()
-			     .addField("lot_canine_uuid", TypeDescription.createString())
-			     .addField("manufacturer_lot_number", TypeDescription.createString())
-			     .addField("lot_filled_date", TypeDescription.createTimestamp())
-
-
-			     );
-
-			mapOrcSchemas.put("map_lot_canine_supplier_blockchain|1.0", TypeDescription.createStruct()
-			     .addField("map_lot_canine_supplier_blockchain_uuid", TypeDescription.createString())
-			     .addField("lot_canine_uuid", TypeDescription.createString())
-			     .addField("supplier_blockchain_uuid", TypeDescription.createString())
-			     .addField("ingredient_sequence", TypeDescription.createInt())
-			     .addField("ingredient_name", TypeDescription.createString())
-
-
-			     );
+		mapOrcSchemas.put("supplier|1.0", TypeDescription.fromString("struct<supplier_uuid:STRING,duns_number:STRING,supplier_name:STRING,supplier_category:STRING,supplier_sub_category:STRING,state_province:STRING,country:STRING,encoded_public_key:STRING>"));
+		mapOrcSchemas.put("supplier_blockchain|1.0", TypeDescription.fromString("struct<supplier_blockchain_uuid:STRING,supplier_type:STRING>"));
+		mapOrcSchemas.put("supplier_block|1.0", TypeDescription.fromString("struct<supplier_block_uuid:STRING,supplier_blockchain_uuid:STRING,hash:STRING,previous_hash:STRING,block_timestamp:TIMESTAMP,block_sequence:INT>"));
+		mapOrcSchemas.put("supplier_block_transaction|1.0", TypeDescription.fromString("struct<supplier_block_transaction_uuid:STRING,supplier_block_uuid:STRING,transaction_id:STRING,encoded_public_key_from:STRING,encoded_public_key_to:STRING,signature:BINARY,transaction_sequence:INT>"));
+		mapOrcSchemas.put("supplier_transaction|1.0", TypeDescription.fromString("struct<supplier_transaction_uuid:STRING,supplier_block_transaction_uuid:STRING,supplier_uuid:STRING,supplier_lot_number:STRING,item_number:STRING,description:STRING,qty:INT,units:STRING,shipped_date_iso8601:TIMESTAMP,rcvd_date_iso8601:TIMESTAMP>"));
+		mapOrcSchemas.put("lot_canine|1.0", TypeDescription.fromString("struct<lot_canine_uuid:STRING,manufacturer_lot_number:STRING,lot_filled_date:TIMESTAMP>"));
+		mapOrcSchemas.put("map_lot_canine_supplier_blockchain|1.0", TypeDescription.fromString("struct<map_lot_canine_supplier_blockchain_uuid:STRING,lot_canine_uuid:STRING,supplier_blockchain_uuid:STRING,ingredient_sequence:INT,ingredient_name:STRING>"));
+//
+//		
+//		
+//		mapOrcSchemas.put("supplier|1.0", TypeDescription.createStruct()
+//			     .addField("supplier_uuid", TypeDescription.createString())
+//			     .addField("duns_number", TypeDescription.createString())
+//			     .addField("supplier_name", TypeDescription.createString())
+//			     .addField("supplier_category", TypeDescription.createString())
+//			     .addField("supplier_sub_category", TypeDescription.createString())
+//			     .addField("state_province", TypeDescription.createString())
+//			     .addField("country", TypeDescription.createString())
+//			     .addField("encoded_public_key", TypeDescription.createString())
+//
+//
+//			     );
+//
+//			mapOrcSchemas.put("supplier_blockchain|1.0", TypeDescription.createStruct()
+//			     .addField("supplier_blockchain_uuid", TypeDescription.createString())
+//			     .addField("supplier_type", TypeDescription.createString())
+//
+//
+//			     );
+//
+//			mapOrcSchemas.put("supplier_block|1.0", TypeDescription.createStruct()
+//			     .addField("supplier_block_uuid", TypeDescription.createString())
+//			     .addField("supplier_blockchain_uuid", TypeDescription.createString())
+//			     .addField("hash", TypeDescription.createString())
+//			     .addField("previous_hash", TypeDescription.createString())
+//			     .addField("block_timestamp", TypeDescription.createTimestamp())
+//			     .addField("block_sequence", TypeDescription.createInt())
+//
+//
+//			     );
+//
+//			mapOrcSchemas.put("supplier_block_transaction|1.0", TypeDescription.createStruct()
+//			     .addField("supplier_block_transaction_uuid", TypeDescription.createString())
+//			     .addField("supplier_block_uuid", TypeDescription.createString())
+//			     .addField("transaction_id", TypeDescription.createString())
+//			     .addField("encoded_public_key_from", TypeDescription.createString())
+//			     .addField("encoded_public_key_to", TypeDescription.createString())
+//			     .addField("signature", TypeDescription.createBinary())
+//			     .addField("transaction_sequence", TypeDescription.createInt())
+//
+//
+//			     );
+//
+//			mapOrcSchemas.put("supplier_transaction|1.0", TypeDescription.createStruct()
+//			     .addField("supplier_transaction_uuid", TypeDescription.createString())
+//			     .addField("supplier_block_transaction_uuid", TypeDescription.createString())
+//			     .addField("supplier_uuid", TypeDescription.createString())
+//			     .addField("supplier_lot_number", TypeDescription.createString())
+//			     .addField("item_number", TypeDescription.createString())
+//			     .addField("description", TypeDescription.createString())
+//			     .addField("qty", TypeDescription.createInt())
+//			     .addField("units", TypeDescription.createString())
+//			     .addField("shipped_date_iso8601", TypeDescription.createTimestamp())
+//			     .addField("rcvd_date_iso8601", TypeDescription.createTimestamp())
+//
+//
+//			     );
+//
+//			mapOrcSchemas.put("lot_canine|1.0", TypeDescription.createStruct()
+//			     .addField("lot_canine_uuid", TypeDescription.createString())
+//			     .addField("manufacturer_lot_number", TypeDescription.createString())
+//			     .addField("lot_filled_date", TypeDescription.createTimestamp())
+//
+//
+//			     );
+//
+//			mapOrcSchemas.put("map_lot_canine_supplier_blockchain|1.0", TypeDescription.createStruct()
+//			     .addField("map_lot_canine_supplier_blockchain_uuid", TypeDescription.createString())
+//			     .addField("lot_canine_uuid", TypeDescription.createString())
+//			     .addField("supplier_blockchain_uuid", TypeDescription.createString())
+//			     .addField("ingredient_sequence", TypeDescription.createInt())
+//			     .addField("ingredient_name", TypeDescription.createString())
+//
+//
+//			     );
 
 		
 	}
--- bcsc-service/src/main/java/com/petezybrick/bcsc/service/utils/GenHiveCreateTables.java
+++ bcsc-service/src/main/java/com/petezybrick/bcsc/service/utils/GenHiveCreateTables.java
@@ -10,10 +10,12 @@ import org.apache.commons.io.FileUtils;
 import org.apache.orc.TypeDescription;
 
 public class GenHiveCreateTables {
+	public static final String HIVE_DB_NAME = "db_bcsc";
 	private Map<String,TypeDescription> mapOrcSchemas;
 	private List<String> schemaStmts;
 	private List<String> structStmts;
 	private List<String> hiveCreateTables;
+	private List<String> hiveDropTables;
 	
 	public static void main( String[] args ) {
 		try {
@@ -37,10 +39,14 @@ public class GenHiveCreateTables {
 		schemaStmts = new ArrayList<String>();
 		structStmts = new ArrayList<String>();
 		hiveCreateTables = new ArrayList<String>();
+		hiveDropTables = new ArrayList<String>();
 		List<String> records = FileUtils.readLines(new File(pathSqlCreateTables));
 		String version = "1.0";
 		TypeDescription schema = null;
 		boolean isCreateTable = false;
+		StringBuilder structStmt = null;
+		boolean isFirstColumn = false;
+		String tableName = null;
 		
 		for( String record : records ) {
 			record = record.trim();
@@ -48,36 +54,50 @@ public class GenHiveCreateTables {
 				isCreateTable = true;
 				String[] tokens = record.split("[ ]");
 				String tableNameVersion = tokens[2] + "|" + version;
+				tableName = tokens[2];
 				schemaStmts.add("mapOrcSchemas.put(\"" + tableNameVersion + "\", TypeDescription.createStruct()" );
-				structStmts.add("mapOrcSchemas.put(\"" + tableNameVersion + "\", \"struct<" );
+				structStmt = new StringBuilder().append("mapOrcSchemas.put(\"").append(tableNameVersion)
+						.append( "\", TypeDescription.fromString(\"struct<" ); 
+				isFirstColumn = true;
 				schema = TypeDescription.createStruct();
 				mapOrcSchemas.put(tableNameVersion,  schema );
-				hiveCreateTables.add("CREATE TABLE " + tokens[2] + " ( ");
+				hiveDropTables.add("DROP TABLE " + HIVE_DB_NAME + "." + tableName + ";");
+				hiveCreateTables.add("CREATE TABLE " + HIVE_DB_NAME + "." + tableName + " ( ");
 
 			} else if(record.endsWith(";") ) {
 				if( isCreateTable ) {
 					schemaStmts.add("     );");
 					schemaStmts.add("");
-					hiveCreateTables.add(");");
-					structStmts.add("     >\");");
-					structStmts.add("");
+					hiveCreateTables.add(") STORED AS ORC LOCATION '/user/bcsc/bcsc_data/" + tableName + "';");
+					hiveCreateTables.add("");
+					structStmt.append(">\"));");
+					structStmts.add(structStmt.toString());
 				}
 				isCreateTable = false;
-				
+
 			} else if( isCreateTable ) {
 				String[] tokens = record.split("[ ]");
 				String columnName = tokens[0];
+				if( "insert_ts".equals(columnName) || "update_ts".equals(columnName) ) continue;
 				SchemaPair schemaPair = findTypeDescription( tokens[1].toUpperCase() );
 				schema.addField( columnName, schemaPair.getTypeDescription() );
-				schemaStmts.add("     .addField(\"" + columnName + "\", " + schemaPair.getStatement() + ")" );				
-				structStmts.add( columnName + ":" + findStruct(tokens[1].toUpperCase()) );
-				hiveCreateTables.add("     " + columnName + " TYPE " + ",");
+				schemaStmts.add("     .addField(\"" + columnName + "\", " + schemaPair.getStatement() + ")" );
+				String delim = "";
+				if( !isFirstColumn ) {
+					structStmt.append(",");
+					delim = ",";
+				}
+				else isFirstColumn = false;
+				structStmt.append(columnName).append(":").append(findStruct(tokens[1].toUpperCase()) );
+				hiveCreateTables.add("     " + delim + columnName + " " + findStruct(tokens[1].toUpperCase()));
 			}
 		}
 		
 		schemaStmts.forEach( ss -> System.out.println(ss.toString()));
 		System.out.println();
 		structStmts.forEach( ss -> System.out.println(ss.toString()));
+		System.out.println();
+		hiveDropTables.forEach(hdt -> System.out.println(hdt.toString()));
 		System.out.println();
 		hiveCreateTables.forEach(hct -> System.out.println(hct.toString()));
 //		put("person|1.0", TypeDescription.createStruct()
@@ -107,7 +127,7 @@ public class GenHiveCreateTables {
 	}
 	
 	private String findStruct( String dataType ) throws Exception {
-		if( "INTEGER".equals(dataType)) return "INTEGER";
+		if( "INTEGER".equals(dataType)) return "INT";
 		else if( "BIGINT".equals(dataType) ) return "BIGINT";
 		else if( dataType.startsWith("CHAR") || dataType.startsWith("VARCHAR") ) return "STRING";
 		else if( "DATE".equals(dataType)) return "DATE";
